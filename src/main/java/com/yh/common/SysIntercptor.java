1c9b5dc3094c09745758a2e415eb61ff67588920
Navy <HongHai>
2018-10-10 21:46:37 +0800

    拦截器。。

---
+++ src/main/java/com/yh/common/SysIntercptor.java
@@ -0,0 +1,34 @@
+package com.yh.common;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class SysIntercptor implements HandlerInterceptor {
+	private static final Log logger = LogFactory.getLog(SysIntercptor.class);
+	@Override
+    public boolean preHandle(HttpServletRequest httpServletRequest,HttpServletResponse httpServletResponse, Object o) throws Exception {
+        System.out.println("Interceptor1....preHandle");
+        try{
+        	if("".equals(Common.getSession().getAttribute("userid").toString())||
+            		Common.getSession().getAttribute("userid").toString()==null){
+            	return false;
+            }else
+            	return true;
+        }catch(Exception ex){
+        	logger.info(ex.getMessage());
+        	return false;
+        }
+    }
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+        System.out.println("Interceptor1....postHandle");
+    }
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+        System.out.println("Interceptor1....afterCompletion");
+    }
+}
